ce72f48e9b4cebf9b0f9c51d4836614e72d5f221
ISMINIMIN <summmm.224@gmail.com>
2024-04-24 19:25:56 +0900

    feat: 예외 처리 추가 - HttpMessageNotReadableException

--- src/main/java/minzdev/api/ApiResponse.java
+++ src/main/java/minzdev/api/ApiResponse.java
@@ -33,6 +33,10 @@ public class ApiResponse<T> {
         this.results = List.of(result);
     }
 
+    public ApiResponse(int code, String message) {
+        this.status = new Status(code, message);
+    }
+
     public ApiResponse(int code, String message, Object inputRestriction) {
         this.status = new Status(code, message);
         this.inputRestriction = inputRestriction;
--- src/main/java/minzdev/api/controller/BaseController.java
+++ src/main/java/minzdev/api/controller/BaseController.java
@@ -1,9 +1,14 @@
 package minzdev.api.controller;
 
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import jakarta.servlet.http.HttpServletResponse;
 import minzdev.api.ApiResponse;
 import minzdev.api.exception.CustomException;
+import minzdev.api.exception.ErrorCode;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.List;
 
@@ -16,6 +21,20 @@ abstract public class BaseController {
                 exception.getErrorCode().getCode(),
                 exception.getMessage(),
                 exception.getInputRestriction());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public <T> ApiResponse<T> HttpMessageNotReadableExceptionHandler(HttpMessageNotReadableException exception) {
+        if (exception.getCause() instanceof MismatchedInputException mismatchedInputException) {
+            return new ApiResponse<>(
+                    ErrorCode.BAD_REQUEST.getCode(),
+                    mismatchedInputException.getPath().get(0).getFieldName() + " 필드의 값이 잘못되었습니다.");
+        }
+
+        return new ApiResponse<>(
+                ErrorCode.BAD_REQUEST.getCode(),
+                "확인할 수 없는 형태의 데이터입니다.");
     }
 
     public <T> ApiResponse<T> makeApiResponse(List<T> results) {
